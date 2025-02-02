0b8a5077030c39739888c06c62e393baf8356990
TcDrozd <trcharlesd@gmail.com>
2025-02-02 09:59:04 -0500

    Initial Commit

---
+++ DSWrapper/DSWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSWrapperApp.swift
+//  DSWrapper
+//
+//  Created by Trevor Drozd on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct DSWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DSWrapperTests/DSWrapperTests.swift
@@ -0,0 +1,16 @@
+//
+//  DSWrapperTests.swift
+//  DSWrapperTests
+//
+//  Created by Trevor Drozd on 2/2/25.
+//
+
+import Testing
+
+struct DSWrapperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
